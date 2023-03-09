2383411b55568288d8793b80dd738836da52ed4d
yura <rladbfk0000@naver.com>
2023-03-10 00:16:29 +0900

    comment : 2023-03-10 (1)

--- js/test.js
+++ js/test.js
@@ -211,10 +211,22 @@
 //     return need > money ? need-money : 0;
 // }
 // 2023-03-08 (1)
-function solution(left, right) {
-  var answer = 0;
-  for (let i = left; i <= right; i++) {
-    Math.sqrt(i) % 1 == 0 ? (answer -= i) : (answer += i);
+// function solution(left, right) {
+//   var answer = 0;
+//   for (let i = left; i <= right; i++) {
+//     Math.sqrt(i) % 1 == 0 ? (answer -= i) : (answer += i);
+//   }
+//   return answer;
+// }
+
+// 2023-03-10(1)
+function solution(s){
+  let total=0;
+  for (let i =0; i<s.length; i++){
+      s[i]=="("? total+=1 : total-=1
+          if(total<0){
+              break;
   }
-  return answer;
 }
+      return total==0 ? true : false
+  }
